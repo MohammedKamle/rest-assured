3fcd1702e05e20105b25c4855294860f0e144c5e
Mohammed Kamle <mohammed@Mohammeds-MacBook-Pro.local>
2021-11-07 12:35:49 +0530

    added 8 and 8

---
+++ src/test/java/com/rest/ResponseSpecBuilder_8.java
@@ -0,0 +1,56 @@
+package com.rest;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+
+/*
+ * For Validating repetative response assertions
+ * */
+public class ResponseSpecBuilder_8 {
+    ResponseSpecification responseSpecification;
+
+    @BeforeClass
+    public void beforeClass(){
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setBaseUri("https://api.postman.com");
+        requestSpecBuilder.addHeader("X-Api-key", "PMAK-616285d31f6ba90052628ba4-4cf7a27ad04573552bca07f02e2dcc2c32");
+        RestAssured.requestSpecification = requestSpecBuilder.build();
+
+        // for code reusability
+//        responseSpecification = RestAssured.expect()
+//                .statusCode(200)
+//                .contentType(ContentType.JSON);
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .log(LogDetail.ALL);
+
+        responseSpecification = responseSpecBuilder.build();
+    }
+
+    @Test
+    public void validate_status_code(){
+        get("/workspaces")
+                .then().spec(responseSpecification);
+    }
+
+    @Test
+    public void validate_response_body(){
+        Response response = get("/workspaces")
+                .then().spec(responseSpecification)
+                .extract().response();
+        assertThat(response.path("workspaces[0].name"), equalTo("My Workspace 2"));
+    }
+}

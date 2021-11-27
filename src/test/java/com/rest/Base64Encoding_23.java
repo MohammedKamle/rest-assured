b2407c3dcf87110a5a3ff8f3ce3a6779a862c3c1
Mohammed Kamle <mohammed@Mohammeds-MacBook-Pro.local>
2021-11-27 09:37:36 +0530

    addedn base64 encoding sample

---
+++ src/test/java/com/rest/Base64Encoding_23.java
@@ -0,0 +1,13 @@
+package com.rest;
+
+import java.util.Base64;
+
+public class Base64Encoding_23 {
+    public static void main(String[] args) {
+        String usernamePassword = "myUsername:myPassword";
+        String base64Encoded = Base64.getEncoder().encodeToString(usernamePassword.getBytes());
+        System.out.println("Encoded value :: "+base64Encoded);
+        byte[] decodedBytes = Base64.getDecoder().decode(base64Encoded);
+        System.out.println("Decoded bytes :: "+new String(decodedBytes));
+    }
+}

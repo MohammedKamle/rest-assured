dd36944bfe98518b7ec33f7cf5a657818a3d9323
Mohammed Kamle <mohammed@Mohammeds-MacBook-Pro.local>
2021-12-01 12:29:50 +0530

    added base64 sample

--- src/test/java/com/rest/Base64Encoding_23.java
+++ src/test/java/com/rest/Base64Encoding_23.java
@@ -9,5 +9,10 @@ public class Base64Encoding_23 {
         System.out.println("Encoded value :: "+base64Encoded);
         byte[] decodedBytes = Base64.getDecoder().decode(base64Encoded);
         System.out.println("Decoded bytes :: "+new String(decodedBytes));
+
+        String s = new String("2");
+        System.out.println("");
+       // System.out.println(2 + 20 + s + 2 + 20);
+
     }
 }
